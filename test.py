01c5377dde89c0c07fff27cbd46c9bf2c634e199
Piyawan00 <0893425618.aa@gmail.com>
2022-10-26 21:10:59 +0700

    up

---
+++ test.py
@@ -0,0 +1,8 @@
+    connection = psycopg2.connect(user="webadmin",
+                                password="TMQkmy54914",
+                                host="node36962-pawaris.proen.app.ruk-com.cloud",
+                                port="11254",
+                                database="postgres")
+
+
+result = .query.all()
